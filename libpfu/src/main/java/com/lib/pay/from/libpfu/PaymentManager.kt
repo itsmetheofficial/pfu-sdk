108e72ed9900bfca76e01d9e48dd45272b8891f9
Muhammad Kamran <mkami.kamran786@gmail.com>
2025-05-11 16:41:22 +0500

    default dialog

--- libpfu/src/main/java/com/lib/pay/from/libpfu/PaymentManager.kt
+++ libpfu/src/main/java/com/lib/pay/from/libpfu/PaymentManager.kt
@@ -7,6 +7,6 @@ import com.lib.pay.from.libpfu.callbacks.PaymentCallbacks
 interface PaymentManager {
     fun initialize(activity: ComponentActivity)
     fun initialize(activity: FragmentActivity)
-    fun setCallbacks(callbacks: PaymentCallbacks,showBuiltInDialog:Boolean=true)
+    fun setCallbacks(callbacks: PaymentCallbacks,showBuiltInDialog:Boolean=false)
     suspend fun createTransaction(bearerToken: String,userName:String,email:String,mobile:String,amount:Int,redirectUrl:String="",webhookUrlId:Int=1)
 }
